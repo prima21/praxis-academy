aba5d11af289f079a95e14bc3be2edd21056ea0b
prima21 <prima.ananda21@gmail.com>
2020-02-06 16:06:14 +0700

    commit 01-04

---
+++ novice/01-04/argument.js
@@ -0,0 +1,6 @@
+function perkalian(a,b){
+    let hitung;
+    hitung = (a*b);
+    return hitung;
+}
+console.log (perkalian(3,2));
---
+++ novice/01-04/function.js
@@ -0,0 +1,7 @@
+function kubus(a,b){
+    let total;
+    total= ((a**3)+(b**3));
+
+    return total;
+}
+console.log(kubus(8,3));
